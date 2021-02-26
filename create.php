43fa01bd2f8d61aa795a44592afe3fc62ce34243
Luke.Deng <xiangshuo1992@gmail.com[D>
2021-02-26 14:18:50 +0800

    姓名不为空，第二次提交

--- create.php
+++ create.php
@@ -14,9 +14,11 @@ $data = $res->fetchAll();
 </head>
 <body>
     <h2>学生添加</h2>
-    <form action="createdo.php" method="POST" enctype="multipart/form-data">
+    <form action="createdo.php" method="POST" enctype="multipart/form-data" onsubmit="return checkAll()">
         学生姓名<br>
-        <input type="text" name="s_name"><br>
+        <input type="text" name="s_name" id="name" placeholder="姓名不能为空">
+        <span id="sp_name"></span>
+        <br>
         学生班级<br>
         <select name="c_id">
             <?php foreach($data as $k=>$v){?>
@@ -28,7 +30,27 @@ $data = $res->fetchAll();
         <input type="file" name="s_tu"><br>
         学上简介<br>
         <textarea name="s_jj" id="" cols="30" rows="10"></textarea><br>
-        <input type="submit" value="添加">
+        <input type="submit" id="btn" value="添加">
     </form>
 </body>
+<script src="jquery-3.5.1.min.js"></script>
+<script>
+    $("#btn").on("click",function(){
+        var name = $("#name").val()
+        if(name == ''){
+            $("#sp_name").html('姓名必填')
+            return false
+        }else{
+            return true
+        }
+        function checkAll(){
+        if(name){
+            return true
+        }else{
+            return false
+        }
+    }
+    })
+    
+</script>
 </html>
